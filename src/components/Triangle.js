e9129c62376ce3d02ea4c8f5b389b279aa6a085a
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-03-23 00:30:57 +0200

    draw tringle centered

--- src/components/Triangle.js
+++ src/components/Triangle.js
@@ -1,19 +1,39 @@
 export default class Triangle {
   constructor({ ctx }) {
     this.ctx = ctx;
-
+    this.height = 100;
+    this.sideWidth = 110;
+    this.topPoint = { x: 0, y: 0 };
+    this.leftPoint = { x: 0, y: 0 };
+    this.rigthPoint = { x: 0, y: 0 };
+    this._calcCoordinates();
     this.draw = this.draw.bind(this);
   }
 
   draw() {
     this.ctx.beginPath();
-    this.ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    this.ctx.moveTo(110, 75);
-    this.ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-    this.ctx.moveTo(65, 65);
-    this.ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-    this.ctx.moveTo(95, 65);
-    this.ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+    this.ctx.moveTo(this.topPoint.x, this.topPoint.y);
+    this.ctx.lineTo(this.leftPoint.x, this.leftPoint.y);
+    this.ctx.lineTo(this.rigthPoint.x, this.rigthPoint.y);
+    this.ctx.closePath();
     this.ctx.stroke();
+  }
+
+  _calcCoordinates() {
+    let centerX = this.ctx.canvas.width / 2;
+    let centerY = this.ctx.canvas.height / 2;
+
+    this.topPoint.x = centerX;
+    this.topPoint.y = centerY - this.height / 3 * 2;
+
+    let cathetus = this.height;
+    let hypotenuse = this.sideWidth;
+    let cathetus2 = Math.sqrt(hypotenuse ** 2 - cathetus ** 2);
+
+    this.leftPoint.x = this.topPoint.x - cathetus2;
+    this.leftPoint.y = this.topPoint.y + hypotenuse;
+
+    this.rigthPoint.x = this.topPoint.x + cathetus2;
+    this.rigthPoint.y = this.topPoint.y + hypotenuse;
   }
 }
